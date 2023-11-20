6257e4fb2fbf974e4af229d6851dfc2b8f049461
allpha23 <allpha23@gmail.com>
2023-11-20 20:54:07 -0300

    feat: Adiciona layout da pagina
    adiciona ao App.tsx: Navbar, Outlet e Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.css';
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      App
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
   );
 }
