8c39239abfbb66029b2d57d561c28984b47129eb
allpha23 <allpha23@gmail.com>
2023-11-21 12:55:26 -0300

    feat: Inicia pagina de Account

---
+++ src/pages/Account.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Account() {
+  return (
+    <div className="bg-zinc-200 h-screen">
+      <h2 className="py-7 text-center text-2xl font-bold">Em desenvolvimento</h2>
+    </div>
+  );
+}
