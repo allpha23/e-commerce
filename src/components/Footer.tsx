9caf89b9094130430263a6ad952ccbdf8dd8b9cc
allpha23 <allpha23@gmail.com>
2023-11-20 21:00:55 -0300

    feat: Adiciona Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className="text-center text-zinc-50 py-3 bg-zinc-950">
+      <p>
+        &copy;2023 by
+        {' '}
+        <span>Pedro W. Soares</span>
+      </p>
+    </div>
+  );
+}
