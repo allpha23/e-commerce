7fe9ae0f211cbe5d29b62c2aa0f9a2f2c41c9ef8
allpha23 <allpha23@gmail.com>
2023-11-20 21:09:16 -0300

    feat: Adiciona use-axios
    adiciona hook customizado para facilitar a requisição a api

---
+++ src/hooks/use-axios.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import { AxiosInstance, AxiosError } from 'axios';
+
+interface ItemsParams {
+  limit: number,
+  offset?: number,
+}
+
+interface Params {
+  params?: ItemsParams
+}
+
+export type RequestConfig = {
+  axiosInstance: AxiosInstance,
+  url: string,
+  othersConfig?: Params,
+};
+
+export default function useAxios(requests: RequestConfig) {
+  const [data, setData] = useState<any | null >(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+  const { axiosInstance, url, othersConfig } = requests;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axiosInstance.get(url, { ...othersConfig });
+        setData(res.data);
+      } catch (err) {
+        const typeError = err as AxiosError;
+        setError(typeError?.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [othersConfig?.params?.offset]);
+
+  return [data, loading, error];
+}
