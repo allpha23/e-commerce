235b59079072bcf7c7ef01c2f1dde865fe9e4e98
allpha23 <allpha23@gmail.com>
2023-11-21 00:00:16 -0300

    feat: Adiciona a pagina Home
    adiciona a pagina Home.tsx que usa os components Featured e NewProducts

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Featured from '../components/Featured';
+import NewProducts from '../components/NewProducts';
+
+export default function Home() {
+  return (
+    <>
+      <Featured />
+      <NewProducts />
+    </>
+  );
+}
