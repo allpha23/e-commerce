8fb00609cd8a7f93053c2a26278b4ed25086c2e2
allpha23 <allpha23@gmail.com>
2023-11-21 23:51:21 -0300

    refactor: Importa interface ProductType

--- src/components/NewProducts.tsx
+++ src/components/NewProducts.tsx
@@ -2,13 +2,7 @@ import React from 'react';
 import useAxios from '../hooks/use-axios';
 import axiosInstance from '../helper/axios-instance';
 import ProductCard from './ProductCard';
-
-type ProductData = {
-  id: string,
-  title: string,
-  thumbnail: string,
-  price: number,
-};
+import { ProductType } from '../interfaces';
 
 export default function NewProducts() {
   const [products] = useAxios({
@@ -21,7 +15,7 @@ export default function NewProducts() {
     <div className="bg-zinc-200 px-10 py-7">
       <h2 className="text-3xl font-bold">Acabaram de Chegar</h2>
       <div className="grid grid-cols-2 gap-7 pt-7 md:grid-cols-4">
-        {products?.results.length > 0 && products.results.map((product: ProductData) => (
+        {products?.results.length > 0 && products.results.map((product: ProductType) => (
           <div key={product.id}>
             <ProductCard
               title={product.title}
