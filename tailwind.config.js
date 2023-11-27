f51a29da3e06d2f3cdf6ee1911c67601f06502a1
allpha23 <allpha23@gmail.com>
2023-11-27 15:32:11 -0300

    feat: Adiciona variant child a talwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }) {
+      addVariant('child', '& > *');
+      addVariant('child-hover', '& > *:hover');
+    }
+  ],
 }
 
